d640ddc2c577872d27a9bc789490d61bdab2bfdc
Pedro Velasco Santana <120191699+LeviDooku@users.noreply.github.com>
2023-10-24 20:12:02 +0200

    Update Pomodoro Counter.ino

--- Pomodoro Counter/Pomodoro Counter.ino
+++ Pomodoro Counter/Pomodoro Counter.ino
@@ -1,17 +1,21 @@
 #include <LiquidCrystal.h>
 
+//PINES PANTALLA
 const int rs = 13;
 const int E = 11;
 const int D4 = 5, D5 = 4, D6 = 3, D7 = 2;
 LiquidCrystal lcd(rs,E,D4,D5,D6,D7);
 
+//PIN BUZZER
 const int buzz_pin = 7;
 
+//VARIABLES LÓGICA PROGRAMA
 int sesion = 0;
-int tEstudio = 25;
-int tDescanso = 5;
-int cont = 5;
+int tEstudio;
+int tDescanso;
+int contador = 5; //Aquí seleccionamos número de sesiones
 
+//FUNCIÓN PARA ACCIONAR BUZZER
 void accionarBuzzer(const int nVeces){
   
 	int contBuzz = nVeces;
@@ -19,7 +23,7 @@ void accionarBuzzer(const int nVeces){
     const int buzz_delay = 100;
 
     while (contBuzz > 0){
-      for (int j = 0; j < 2; j++){ // Repite el patrón dos veces
+      for (int j = 0; j < 2; j++){
         for (int i = 0; i < 1000; i++){
           digitalWrite(buzz_pin, HIGH);
           delayMicroseconds(buzz_delay);
@@ -32,77 +36,68 @@ void accionarBuzzer(const int nVeces){
     }
 }
 
+//FUNCIÓN PARA LIMPIAR Y APLICAR DELAY
 void limpiayDelay(const int delayTime){
 	lcd.clear();
   	delay(delayTime);
 }
 
+//FUNCIÓN IMPRIMIR MENSAJES
+void imprimeMensaje(String mensaje, int row, int col){
+	lcd.setCursor(row, col);
+  	lcd.print(mensaje);
+}
+
 void setup(){
   
-  pinMode(buzz_pin, OUTPUT);
-  lcd.begin(16,2);
-
-  lcd.home();
-  lcd.print("Pomodoro Counter");
-
+  pinMode(buzz_pin, OUTPUT); //Configurar como d. salida buzzer
+  lcd.begin(16,2);	//Inicializar pantalla
+	
+  //Imprimir título
+  imprimeMensaje("Pomodoro Counter", 0, 0);
   delay(3000);
   lcd.clear();
+  
 }
 
 void loop() {
 
-    while(cont > 0){
+    while(contador > 0){
 
       sesion++;
+      tEstudio = 25;
+      tDescanso = 5;
+      
+      //Do - while estudio
       
       do{
-
-        lcd.home();
-        lcd.print("Sesion: ");
-        lcd.setCursor(11,0);
-        lcd.print(sesion);
-        lcd.setCursor(0,1);
-        lcd.print("Minutes: ");
-        lcd.setCursor(10, 1);
-
+        imprimeMensaje("Sesion: "+String(sesion), 0, 0);
+        //Evitar errores con un if - else
         if(tEstudio >= 10){
-          lcd.print(tEstudio);
-        }
-        else{
-          lcd.print("0"+String(tEstudio));
+          imprimeMensaje("Minutos: "+String(tEstudio), 0, 1);
+        }else{
+          imprimeMensaje("Minutos: 0"+String(tEstudio), 0, 1);
         }      
-
         tEstudio--;
-        delay(60);
-
+        delay(60000);
       }while(tEstudio != 0);
 
+
       limpiayDelay(500);
-      accionarBuzzer(3);
-
+      accionarBuzzer(2);
+	
+      //Do - while descanso 
       do{
-
-        lcd.home();
-        lcd.print("A descansar");
-        lcd.setCursor(0,1);
-        lcd.print("Minutes: ");
-        lcd.print(tDescanso);
+        imprimeMensaje("A descansar", 0, 0);
+        imprimeMensaje("Minutos: "+String(tDescanso), 0, 1);
         tDescanso--;
-        delay(60);
-
+        delay(60000);
       }while(tDescanso != 0);
 
       limpiayDelay(500);
-      accionarBuzzer(3);
-      cont--;
+      accionarBuzzer(2);
+      contador--; //Decrementamos contador de sesiones
     }
   
-    lcd.home();
-    lcd.print("Estudio");
-    lcd.setCursor(0,1);
-    lcd.print("Terminado! :)");
+	imprimeMensaje("Estudio finished", 0, 0); //Terminamos así el programa
 }
-
-
-
-
